423cebdb4f7eff36579846d6c5de9f7dd77ce0ad
gapbo-iscte <gapbo@iscte-iul.pt>
2021-10-26 21:21:31 +0100

    Converted to Maven project

---
+++ src/main/java/pt/tecsis/es_leti_1sem_2021_grupo15/Main.java
@@ -0,0 +1,5 @@
+package pt.tecsis.es_leti_1sem_2021_grupo15;
+
+public class Main {
+
+}
--- src/pt/tecsis/engenharia_software/ScrumTracker.java
+++
@@ -1,9 +0,0 @@
-package pt.tecsis.engenharia_software;
-
-public class ScrumTracker {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-
-}
