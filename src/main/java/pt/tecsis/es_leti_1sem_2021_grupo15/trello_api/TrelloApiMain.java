0fb43b4d379a933275cd3cf4bce6cec3366956bf
Gonçalo André Benido <gapbo@iscte-iul.pt>
2021-11-12 17:10:41 +0000

    Utilização do novo API trello4j

--- src/main/java/pt/tecsis/es_leti_1sem_2021_grupo15/trello_api/TrelloApiMain.java
+++ src/main/java/pt/tecsis/es_leti_1sem_2021_grupo15/trello_api/TrelloApiMain.java
@@ -2,32 +2,33 @@ package pt.tecsis.es_leti_1sem_2021_grupo15.trello_api;
 
 import java.util.List;
 
-import com.julienvey.trello.Trello;
-import com.julienvey.trello.domain.Board;
-import com.julienvey.trello.domain.TList;
-import com.julienvey.trello.impl.TrelloImpl;
-import com.julienvey.trello.impl.http.ApacheHttpClient;
+import org.trello4j.Trello;
+import org.trello4j.TrelloImpl;
+import org.trello4j.model.Action;
+import org.trello4j.model.Card;
+import org.trello4j.model.Checklist;
 
 
 public class TrelloApiMain {
 	
 	
-	private static final String trelloKey="coloca_a_tua_key";
-	private static final String trelloAccessToken="coloca_o_teu_token";
+	private static final String trelloKey="dab38c0e84b88819e31482cf6017c733";
+	private static final String trelloAccessToken="835f871fa169a6727334a677943a48dbc4369af0ebb26356f9b2c3cc291e1ff0";
 	
 	
 	public static void main(String[] args) {
 	     
-	     Trello trelloApi = new TrelloImpl(trelloKey, trelloAccessToken, new ApacheHttpClient());
+		Trello trelloApi = new TrelloImpl(trelloKey, trelloAccessToken); 
 	   
 	     
-	    List<Board> boards = TrelloQuadros.getNomeDoProjeto(trelloApi);
-	    List<TList> quadros = TrelloQuadros.getQuadros(trelloApi,boards.get(0));
+	    //List<Board> boards = TrelloQuadros.getNomeDoProjeto(trelloApi);
+	    List<Card> quadros = TrelloQuadros.getQuadros(trelloApi);//,boards.get(0));
+	    
+		//List<org.trello4j.model.List> checklists =  TrelloQuadros.getList(trelloApi);
 	
+	    List<Action> actions = TrelloQuadros.getActions(trelloApi, quadros.get(0).getId());
 	
-	
-	
-	
+
 	
 	}
 	
--- src/main/java/pt/tecsis/es_leti_1sem_2021_grupo15/trello_api/TrelloQuadros.java
+++ src/main/java/pt/tecsis/es_leti_1sem_2021_grupo15/trello_api/TrelloQuadros.java
@@ -1,38 +1,38 @@
 package pt.tecsis.es_leti_1sem_2021_grupo15.trello_api;
 
+
 import java.util.List;
 
-import org.apache.http.HttpResponse;
+import org.trello4j.Trello;
+import org.trello4j.model.Action;
+import org.trello4j.model.Board;
+import org.trello4j.model.Card;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.julienvey.trello.Trello;
-import com.julienvey.trello.domain.Board;
-import com.julienvey.trello.domain.TList;
 
 
 public class TrelloQuadros {
 	
-	
+	//Requerimento é feito com os dados da conta do utilizador (EX:Projeto_ES)
 	public static List<Board> getNomeDoProjeto(Trello trelloApi) {
 		
-		List<Board> member = trelloApi.getMemberBoards("utilizador_trello");{
+		//List<Board> member = trelloApi.//getMemberBoards("utilizador_trello");{
 	     /*for (Board  quadro: member) {
 		  System.out.println(quadro.getName()+ "-" +quadro.getId());
 	    }*/
-		
-			System.out.println(member.get(0).getName()+ "-" + member.get(0).getId());
+		List<Board> boards = trelloApi.getBoardsByMember("goncalobenido");
+			System.out.println(boards.get(0).getName() + "-" + boards.get(0).getId() );
 			
-		}
+		//}
 		
-		return member;
+		return boards;
 	}
 	
 	
-	
-	public static List<TList> getQuadros(Trello trelloApi, Board projectName) {
-			
+	//Requerimento é feito com os ID do Board (EX:Reuniões, Daily Scrum, etc...)
+	public static List<org.trello4j.model.List> getList(Trello trelloApi){//, //Board projectName) {
+		
 		//Board board;
-		List<TList> lists = projectName.fetchLists();
+		/*List<TList> lists = projectName.fetchLists();
 	     
 	     for (Board  quadro: getNomeDoProjeto(trelloApi) ) {
 	         projectName = trelloApi.getBoard(quadro.getId());
@@ -41,13 +41,65 @@ public class TrelloQuadros {
 	            // System.out.println(board.getName()+"- "+ board.getId()+"-"+board.getIdBoard());
 	         }
 	     
-	     }
-		return lists;
-	     
-	     
-	    
+	     }*/
+		
+		List<Board> board= getNomeDoProjeto(trelloApi);
+		
+		List<org.trello4j.model.List> checklists = trelloApi. getListByBoard(board.get(0).getId());
+		
+		/*for (org.trello4j.model.List cartas : checklists) {
+            System.out.println(cartas.getName());//+"- "+ cartas.getId()+"-"+cartas.getIdBoard());
+		}*/
+		
+		return checklists;
 		
 		
+	}
+	
+	
+	
+	//Requerimento é feito com os ID da Lista (EX:[R0] - Entendimento do projeto e configurações iniciais)
+	public static  List<Card> getQuadros(Trello trelloApi){  //, //Board projectName) {
+			
+		//Board board;
+		/*List<TList> lists = projectName.fetchLists();
+	     
+	     for (Board  quadro: getNomeDoProjeto(trelloApi) ) {
+	         projectName = trelloApi.getBoard(quadro.getId());
+	         for (TList lista : lists) {
+	             System.out.println(lista.getName()+"- "+ lista.getId()+"-"+lista.getIdBoard());
+	            // System.out.println(board.getName()+"- "+ board.getId()+"-"+board.getIdBoard());
+	         }
+	     
+	     }*/
+		
+		 List<org.trello4j.model.List> lista = getList(trelloApi);
+		
+		List<Card> cards = trelloApi.getCardsByList(lista.get(0).getId());
+
+		for (Card cartas : cards) {
+            System.out.println(cartas.getName()+"- "+ cartas.getId());//+"-"+cartas.getIdBoard());
+		}
+		
+		return cards;
+	     
+	     
+	}
+	
+	
+	
+	public static  List<Action> getActions(Trello trelloApi, String cartaId ){
+			
+		
+		
+		List<Action> actions = trelloApi.getActionsByCard(cartaId);
+		
+		for (Action action : actions) {
+            System.out.println(action.getMemberCreator().getUsername()+"-"+action.getData().getText());
+		}
+		
+		
+		return actions;  	
 	}
 	
 	
