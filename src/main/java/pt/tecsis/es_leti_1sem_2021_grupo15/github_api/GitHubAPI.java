593250b1276846180d7fb1e9f4f6e8271144b0af
Manuel Covas <manuel.p.covas@gmail.com>
2021-11-30 02:51:53 +0000

    Implementada a chamada GET repos/<owner>/<repository>/tags do API do GitHub. Implementada a chamada GET /repos/<owner>/<repo>/commits/<ref> do API do GitHub. Adicionadas as classes GitHubCommit e GitHubTag.

--- src/main/java/pt/tecsis/es_leti_1sem_2021_grupo15/github_api/GitHubAPI.java
+++ src/main/java/pt/tecsis/es_leti_1sem_2021_grupo15/github_api/GitHubAPI.java
@@ -5,6 +5,7 @@ import java.util.Base64;
 
 import javax.naming.AuthenticationException;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import okhttp3.OkHttpClient;
@@ -67,5 +68,93 @@ public class GitHubAPI {
 		String base64Content = fileObject.getString("content").replaceAll("\n", "");
 		
 		return new String(Base64.getDecoder().decode(base64Content));
+	}
+	
+	
+	/**
+	 * Devolve o commit correspondente ao URL indicado.
+	 * @param  commitURL - url do commit pretendido ({@link String})
+	 * @param  credentials - credenciais a utilizar ou {@code null} para aceder sem credenciais ({@link GitHubCredentials})
+	 * @return o commit requisitado ({@link GitHubCommit})
+	 * @throws IOException
+	 * @throws AuthenticationException
+	 */
+	
+	public static GitHubCommit getCommit(String commitURL, GitHubCredentials credentials) throws IOException, AuthenticationException {
+		
+		Request.Builder requestBuilder = new Request.Builder().url(commitURL).addHeader("Accept", "application/json");
+		Response response;
+		
+		if (credentials == null) {
+			Request request = requestBuilder.build();
+			response = new OkHttpClient().newCall(request).execute();
+		} else {
+			Request request = credentials.authenticateRequest(requestBuilder).build();
+			response = credentials.getHttpClient().newCall(request).execute();
+		}
+		
+		if (response.code() != 200)
+			throw new AuthenticationException("Obtenção do commit de URL " + commitURL + " falhou: HTTP Status " + response.code() + " " + response.message());
+		
+		return new GitHubCommit(response.body().string());
+	}
+	
+	
+	/**
+	 * Devolve o commit pretendido.
+	 * @param  owner - nome de utilizador do dono do repositório ({@link String})
+	 * @param  repository - nome do repositório ({@link String})
+	 * @param  commitHash - hash do commit pretendido ({@link String})
+	 * @param  credentials - credenciais a utilizar ou {@code null} para aceder sem credenciais ({@link GitHubCredentials})
+	 * @return o commit requisitado ({@link GitHubCommit})
+	 * @throws IOException
+	 * @throws AuthenticationException
+	 */
+	
+	public static GitHubCommit getCommit(String owner, String repository, String commitHash, GitHubCredentials credentials) throws IOException, AuthenticationException {
+		
+		String commitURL = GithubEndpoints.GET_COMMIT_URL(owner, repository, commitHash);
+		
+		return getCommit(commitURL, credentials);
+	}
+	
+	
+	/**
+	 * Devolve as tags do repositório indicado.
+	 * @param  owner - nome de utilizador do dono do repositório ({@link String})
+	 * @param  repository - nome do repositório ({@link String})
+	 * @param  credentials - credenciais a utilizar ou {@code null} para aceder sem credenciais ({@link GitHubCredentials})
+	 * @return as tags do repositório indicado ({@link String})
+	 * @throws IOException
+	 * @throws AuthenticationException
+	 */
+	
+	public static GitHubTag[] getRepositoryTags(String owner, String repository, GitHubCredentials credentials) throws IOException, AuthenticationException {
+		
+		String commitURL = GithubEndpoints.GET_REPOSITORY_TAGS_URL(owner, repository);
+		
+		Request.Builder requestBuilder = new Request.Builder().url(commitURL).addHeader("Accept", "application/json");
+		Response response;
+		
+		if (credentials == null) {
+			Request request = requestBuilder.build();
+			response = new OkHttpClient().newCall(request).execute();
+		} else {
+			Request request = credentials.authenticateRequest(requestBuilder).build();
+			response = credentials.getHttpClient().newCall(request).execute();
+		}
+		
+		if (response.code() != 200)
+			throw new AuthenticationException("Obtenção do commit de URL " + commitURL + " falhou: HTTP Status " + response.code() + " " + response.message());
+		
+		// Parse response
+		
+		JSONArray responseJSON = new JSONArray(response.body().string());
+		GitHubTag tags[] = new GitHubTag[responseJSON.length()];
+		
+		for (int i = 0; i < tags.length; i++)
+			tags[i] = new GitHubTag(responseJSON.get(i).toString());
+		
+		return tags;
 	}
 }
---
+++ src/main/java/pt/tecsis/es_leti_1sem_2021_grupo15/github_api/GitHubCommit.java
@@ -0,0 +1,29 @@
+package pt.tecsis.es_leti_1sem_2021_grupo15.github_api;
+
+import org.joda.time.DateTime;
+import org.json.JSONObject;
+
+
+public class GitHubCommit {
+
+	public final String hash, htmlURL, message;
+	public final DateTime time;
+	
+	/**
+	 * Interpreta os dados devolvidos pelo GitHub em formato JSON e
+	 * constroi uma instância desta classe com os mesmos.
+	 * @param {@link String} commitJSON
+	 */
+	
+	public GitHubCommit(String commitJSON) {
+		
+		JSONObject json = new JSONObject(commitJSON);
+		JSONObject commitObject = json.getJSONObject("commit");
+		
+		this.hash = json.getString("sha");
+		this.htmlURL = json.getString("html_url");
+		this.message = commitObject.getString("message");
+		this.time = DateTime.parse(commitObject.getJSONObject("author").getString("date"));
+		
+	}
+}
